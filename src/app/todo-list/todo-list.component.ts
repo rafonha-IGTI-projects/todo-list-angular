3fe4ee15a1eb66a157fd995e67ebf4d252fdf4cc
rafonha <johndoe@example.comrafaela.sdp25@gmail.com>
2020-10-06 21:34:53 -0300

    Finish

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
 
-  constructor() { }
+  tasks: TodoItem[] = [
+  ];
+  
+  addTask(description: string) {
+    this.tasks.push({
+      description: description,
+      done: false
+    })
 
-  ngOnInit(): void {
   }
 
+  deleteTask(i: number) {
+    this.tasks.splice(i, 1);
+  }
 }
